6367e22f017fd09e31de16d5e11318f33e557280
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-01-12 12:48:38 +0500

    feat: added configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      cache: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get('database'),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
